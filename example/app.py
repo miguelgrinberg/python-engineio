c02a58795bb133f4eb80e7f4d0c64c3a9281c7c3
Miguel Grinberg <miguelgrinberg50@gmail.com>
2015-08-12 23:02:51 -0700

    minor improvements to the example application

--- example/app.py
+++ example/app.py
@@ -2,7 +2,9 @@ from flask import Flask, render_template
 
 import engineio
 
-eio = engineio.Server(async_mode='threading')
+async_mode = 'threading'
+
+eio = engineio.Server(async_mode=async_mode)
 app = Flask(__name__)
 app.wsgi_app = engineio.Middleware(eio, app.wsgi_app)
 
@@ -29,13 +31,15 @@ def disconnect(sid):
 
 
 if __name__ == '__main__':
-    # deploy with Werkzeug
-    app.run(threaded=True)
-
-    # deploy with eventlet
-    # import eventlet
-    # eventlet.wsgi.server(eventlet.listen(('', 5000)), app)
-
-    # deploy with gevent
-    # from gevent import pywsgi
-    # pywsgi.WSGIServer(('', 5000), app).serve_forever()
+    if async_mode == 'threading':
+        # deploy with Werkzeug
+        app.run(threaded=True)
+    elif async_mode == 'eventlet':
+        # deploy with eventlet
+        import eventlet
+        from eventlet import wsgi
+        wsgi.server(eventlet.listen(('', 5000)), app)
+    elif async_mode == 'gevent':
+        # deploy with gevent
+        from gevent import pywsgi
+        pywsgi.WSGIServer(('', 5000), app).serve_forever()
