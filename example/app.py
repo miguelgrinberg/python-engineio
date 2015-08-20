c6b1ae91e2e7c3ee8dfec3caf8a8dfa8c2800aa2
Miguel Grinberg <miguelgrinberg50@gmail.com>
2015-08-20 00:08:50 -0700

    Make gevent websocket optional in example app

--- example/app.py
+++ example/app.py
@@ -42,6 +42,15 @@ if __name__ == '__main__':
     elif async_mode == 'gevent':
         # deploy with gevent
         from gevent import pywsgi
-        from geventwebsocket.handler import WebSocketHandler
-        pywsgi.WSGIServer(('', 5000), app,
-                          handler_class=WebSocketHandler).serve_forever()
+        try:
+            from geventwebsocket.handler import WebSocketHandler
+            websocket = True
+        except ImportError:
+            websocket = False
+        if websocket:
+            pywsgi.WSGIServer(('', 5000), app,
+                              handler_class=WebSocketHandler).serve_forever()
+        else:
+            pywsgi.WSGIServer(('', 5000), app).serve_forever()
+    else:
+        print('Unknown async_mode: ' + async_mode)
