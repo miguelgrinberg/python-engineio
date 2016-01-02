d1969d6bc016b0e3dc66df7eeb30a9c76debc6b6
Artemiy Rodionov <artemiy.rodionov@gmail.com>
2016-01-02 14:02:36 +0300

    Update tests to correspond with flake8

--- tests/test_async_eventlet.py
+++ tests/test_async_eventlet.py
@@ -17,9 +17,8 @@ class TestServer(unittest.TestCase):
     @mock.patch('engineio.async_eventlet._WebSocketWSGI.__call__',
                 return_value='data')
     def test_wsgi_call(self, _WebSocketWSGI):
-        _WebSocketWSGI.__call__ = lambda e,s: 'data'
+        _WebSocketWSGI.__call__ = lambda e, s: 'data'
         environ = {"eventlet.input": None}
         start_response = "bar"
         wsgi = async_eventlet.WebSocketWSGI(None)
         self.assertEqual(wsgi(environ, start_response), 'data')
-
