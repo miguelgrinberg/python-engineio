12532c09274668adfb518c85f8428bec3eb49e99
Miguel Grinberg <miguelgrinberg50@gmail.com>
2016-03-05 23:24:08 -0800

    Release 0.9.0

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('README.rst', 'r') as f:
 
 setup(
     name='python-engineio',
-    version='0.8.8',
+    version='0.9.0',
     url='http://github.com/miguelgrinberg/python-engineio/',
     license='MIT',
     author='Miguel Grinberg',
