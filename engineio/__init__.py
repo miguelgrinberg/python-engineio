45a7e21516eefc019f9b506b3d41c5d4603a2f24
Miguel Grinberg <miguel.grinberg@gmail.com>
2018-05-12 14:25:03 -0400

    Release 2.1.1

--- engineio/__init__.py
+++ engineio/__init__.py
@@ -7,7 +7,7 @@ if sys.version_info >= (3, 5):  # pragma: no cover
 else:  # pragma: no cover
     AsyncServer = None
 
-__version__ = '2.1.0'
+__version__ = '2.1.1'
 
 __all__ = ['__version__', 'Middleware', 'Server']
 if AsyncServer is not None:  # pragma: no cover
