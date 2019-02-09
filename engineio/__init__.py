f9f88a7f57d0dbea35d1773ef7abeb5ac475b5f6
Miguel Grinberg <miguel.grinberg@gmail.com>
2019-02-09 20:17:33 +0000

    Releaes 3.3.1

--- engineio/__init__.py
+++ engineio/__init__.py
@@ -14,7 +14,7 @@ else:  # pragma: no cover
     get_tornado_handler = None
     ASGIApp = None
 
-__version__ = '3.3.0'
+__version__ = '3.3.1'
 
 __all__ = ['__version__', 'Server', 'WSGIApp', 'Middleware', 'Client']
 if AsyncServer is not None:  # pragma: no cover
