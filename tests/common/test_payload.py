e895d6f5d0fa88566df61ffd316ce31d7cecfb90
Miguel Grinberg <miguel.grinberg@gmail.com>
2020-11-28 20:49:55 +0000

    v4 protocol: new payload separator

--- tests/common/test_payload.py
+++ tests/common/test_payload.py
@@ -11,87 +11,59 @@ class TestPayload(unittest.TestCase):
     def test_encode_empty_payload(self):
         p = payload.Payload()
         assert p.packets == []
-        assert p.encode() == b''
+        assert p.encode() == ''
 
     def test_decode_empty_payload(self):
-        p = payload.Payload(encoded_payload=b'')
-        assert p.encode() == b''
+        p = payload.Payload(encoded_payload='')
+        assert p.encode() == ''
 
-    def test_encode_payload_xhr2(self):
+    def test_encode_payload_text(self):
         pkt = packet.Packet(packet.MESSAGE, data=six.text_type('abc'))
         p = payload.Payload([pkt])
         assert p.packets == [pkt]
-        assert p.encode() == b'\x00\x04\xff4abc'
+        assert p.encode() == '4abc'
 
-    def test_decode_payload_xhr2(self):
-        p = payload.Payload(encoded_payload=b'\x00\x04\xff4abc')
-        assert p.encode() == b'\x00\x04\xff4abc'
-
-    def test_encode_payload_xhr_text(self):
+    def test_encode_payload_text_multiple(self):
         pkt = packet.Packet(packet.MESSAGE, data=six.text_type('abc'))
+        pkt2 = packet.Packet(packet.MESSAGE, data=six.text_type('def'))
+        p = payload.Payload([pkt, pkt2])
+        assert p.packets == [pkt, pkt2]
+        assert p.encode() == '4abc\x1e4def'
+
+    def test_encode_payload_binary(self):
+        pkt = packet.Packet(packet.MESSAGE, data=b'\x00\x01\x02')
         p = payload.Payload([pkt])
         assert p.packets == [pkt]
-        assert p.encode(b64=True) == b'4:4abc'
+        assert p.encode() == 'bAAEC'
 
-    def test_decode_payload_xhr_text(self):
-        p = payload.Payload(encoded_payload=b'4:4abc')
-        assert p.encode() == b'\x00\x04\xff4abc'
+    def test_encode_payload_binary_multiple(self):
+        pkt = packet.Packet(packet.MESSAGE, data=b'\x00\x01\x02')
+        pkt2 = packet.Packet(packet.MESSAGE, data=b'\x03\x04\x05\x06')
+        p = payload.Payload([pkt, pkt2])
+        assert p.packets == [pkt, pkt2]
+        assert p.encode() == 'bAAEC\x1ebAwQFBg=='
 
-    def test_encode_payload_xhr_binary(self):
-        pkt = packet.Packet(packet.MESSAGE, data=b'\x00\x01\x02', binary=True)
-        p = payload.Payload([pkt])
-        assert p.packets == [pkt]
-        assert p.encode(b64=True) == b'6:b4AAEC'
-
-    def test_decode_payload_xhr_binary(self):
-        p = payload.Payload(encoded_payload=b'6:b4AAEC')
-        assert p.encode() == b'\x01\x04\xff\x04\x00\x01\x02'
+    def test_encode_payload_text_binary_multiple(self):
+        pkt = packet.Packet(packet.MESSAGE, data=six.text_type('abc'))
+        pkt2 = packet.Packet(packet.MESSAGE, data=b'\x03\x04\x05\x06')
+        p = payload.Payload([pkt, pkt2, pkt2, pkt])
+        assert p.packets == [pkt, pkt2, pkt2, pkt]
+        assert p.encode() == '4abc\x1ebAwQFBg==\x1ebAwQFBg==\x1e4abc'
 
     def test_encode_jsonp_payload(self):
         pkt = packet.Packet(packet.MESSAGE, data=six.text_type('abc'))
         p = payload.Payload([pkt])
         assert p.packets == [pkt]
-        assert p.encode(jsonp_index=233) == b'___eio[233]("\x00\x04\xff4abc");'
-        assert p.encode(jsonp_index=233, b64=True) == b'___eio[233]("4:4abc");'
+        assert p.encode(jsonp_index=233) == '___eio[233]("4abc");'
 
     def test_decode_jsonp_payload(self):
-        p = payload.Payload(encoded_payload=b'd=4:4abc')
-        assert p.encode() == b'\x00\x04\xff4abc'
+        p = payload.Payload(encoded_payload='d=4abc')
+        assert p.encode() == '4abc'
 
     def test_decode_invalid_payload(self):
         with pytest.raises(ValueError):
-            payload.Payload(encoded_payload=b'bad payload')
+            payload.Payload(encoded_payload='bad payload')
 
-    # performance improvements in the payload parser assume packets in a
-    # payload are either all binary or all text, so the following test does
-    # no work anymore.
-    #
-    # def test_decode_multi_payload(self):
-    #     p = payload.Payload(encoded_payload=b'4:4abc\x00\x04\xff4def')
-    #     self.assertEqual(len(p.packets), 2)
-    #     self.assertEqual(p.packets[0].data, 'abc')
-    #     self.assertEqual(p.packets[1].data, 'def')
-
-    def test_decode_multi_binary_payload(self):
-        p = payload.Payload(
-            encoded_payload=b'\x00\x04\xff4abc\x00\x04\xff4def'
-        )
-        assert len(p.packets) == 2
-        assert p.packets[0].data == 'abc'
-        assert p.packets[1].data == 'def'
-
-    def test_decode_multi_text_payload(self):
-        p = payload.Payload(encoded_payload=b'4:4abc4:4def')
-        assert len(p.packets) == 2
-        assert p.packets[0].data == 'abc'
-        assert p.packets[1].data == 'def'
-
-    def test_decode_multi_binary_payload_with_too_many_packets(self):
+    def test_decode_multi_payload_with_too_many_packets(self):
         with pytest.raises(ValueError):
-            payload.Payload(
-                encoded_payload=b'\x00\x04\xff4abc\x00\x04\xff4def' * 9
-            )
-
-    def test_decode_multi_text_payload_with_too_many_packets(self):
-        with pytest.raises(ValueError):
-            payload.Payload(encoded_payload=b'4:4abc4:4def' * 9)
+            payload.Payload(encoded_payload='4abc\x1e4def\x1e' * 9 + '6')
