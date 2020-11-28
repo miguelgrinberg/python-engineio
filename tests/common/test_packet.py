38beea5f7903af2e79ab3654b0c299aba2f226da
Miguel Grinberg <miguel.grinberg@gmail.com>
2020-11-28 20:06:21 +0000

    v4 protocol: new binary encoding format

--- tests/common/test_packet.py
+++ tests/common/test_packet.py
@@ -1,5 +1,6 @@
 import unittest
 
+import pytest
 import six
 
 from engineio import packet
@@ -11,11 +12,11 @@ class TestPacket(unittest.TestCase):
         assert pkt.packet_type == packet.NOOP
         assert pkt.data is None
         assert not pkt.binary
-        assert pkt.encode() == b'6'
+        assert pkt.encode() == '6'
 
     def test_decode_default_packet(self):
-        pkt = packet.Packet(encoded_packet=b'6')
-        assert pkt.encode(), b'6'
+        pkt = packet.Packet(encoded_packet='6')
+        assert pkt.encode() == '6'
 
     def test_encode_text_packet(self):
         data = six.text_type('text')
@@ -23,43 +24,32 @@ class TestPacket(unittest.TestCase):
         assert pkt.packet_type == packet.MESSAGE
         assert pkt.data == data
         assert not pkt.binary
-        assert pkt.encode() == b'4text'
+        assert pkt.encode() == '4text'
 
     def test_decode_text_packet(self):
         pkt = packet.Packet(encoded_packet=b'4text')
         assert pkt.encode() == b'4text'
 
     def test_encode_binary_packet(self):
-        pkt = packet.Packet(packet.MESSAGE, data=b'\x01\x02\x03', binary=True)
-        assert pkt.packet_type == packet.MESSAGE
-        assert pkt.data == b'\x01\x02\x03'
-        assert pkt.binary
-        assert pkt.encode() == b'\x04\x01\x02\x03'
-
-    def test_encode_binary_bytearray_packet(self):
-        pkt = packet.Packet(
-            packet.MESSAGE, data=bytearray(b'\x01\x02\x03'), binary=True
-        )
-        assert pkt.packet_type == packet.MESSAGE
-        assert pkt.data == b'\x01\x02\x03'
-        assert pkt.binary
-        assert pkt.encode() == b'\x04\x01\x02\x03'
-
-    def test_encode_binary_b64_packet(self):
-        pkt = packet.Packet(
-            packet.MESSAGE, data=b'\x01\x02\x03\x04', binary=True
-        )
-        assert pkt.packet_type == packet.MESSAGE
-        assert pkt.data == b'\x01\x02\x03\x04'
-        assert pkt.binary
-        assert pkt.encode(b64=True) == b'b4AQIDBA=='
-
-    def test_encode_binary_packet_py3(self):
         pkt = packet.Packet(packet.MESSAGE, data=b'\x01\x02\x03')
         assert pkt.packet_type == packet.MESSAGE
         assert pkt.data == b'\x01\x02\x03'
         assert pkt.binary
-        assert pkt.encode() == b'\x04\x01\x02\x03'
+        assert pkt.encode() == b'\x01\x02\x03'
+
+    def test_encode_binary_bytearray_packet(self):
+        pkt = packet.Packet(packet.MESSAGE, data=bytearray(b'\x01\x02\x03'))
+        assert pkt.packet_type == packet.MESSAGE
+        assert pkt.data == b'\x01\x02\x03'
+        assert pkt.binary
+        assert pkt.encode() == b'\x01\x02\x03'
+
+    def test_encode_binary_b64_packet(self):
+        pkt = packet.Packet(packet.MESSAGE, data=b'\x01\x02\x03\x04')
+        assert pkt.packet_type == packet.MESSAGE
+        assert pkt.data == b'\x01\x02\x03\x04'
+        assert pkt.binary
+        assert pkt.encode(b64=True) == 'bAQIDBA=='
 
     def test_decode_binary_packet(self):
         pkt = packet.Packet(encoded_packet=b'\x04\x01\x02\x03')
@@ -79,8 +69,8 @@ class TestPacket(unittest.TestCase):
         assert pkt.data == {'a': 123, 'b': '456'}
         assert not pkt.binary
         assert pkt.encode() in [
-            b'4{"a":123,"b":"456"}',
-            b'4{"b":"456","a":123}',
+            '4{"a":123,"b":"456"}',
+            '4{"b":"456","a":123}',
         ]
 
     def test_decode_json_packet(self):
@@ -95,14 +85,18 @@ class TestPacket(unittest.TestCase):
         assert pkt.packet_type == packet.MESSAGE
         assert pkt.data == 123
         assert not pkt.binary
-        assert pkt.encode() == b'4123'
+        assert pkt.encode() == '4123'
 
     def test_decode_number_packet(self):
-        pkt = packet.Packet(encoded_packet=b'4123')
+        pkt = packet.Packet(encoded_packet='4123')
         assert pkt.packet_type == packet.MESSAGE
         # integer payloads are parsed as strings, see
         # https://github.com/miguelgrinberg/python-engineio/issues/75
         # for background on this decision
         assert pkt.data == '123'
         assert not pkt.binary
-        assert pkt.encode() == b'4123'
+        assert pkt.encode() == '4123'
+
+    def test_binary_non_message_packet(self):
+        with pytest.raises(ValueError):
+            packet.Packet(packet.NOOP, b'\x01\x02\x03')
